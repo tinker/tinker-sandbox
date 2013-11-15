5c293569764a74eb1d7c6e600658716c447ccdac
Chiel Kunkels <chiel@kunkels.me>
2013-11-15 18:49:05 +0000

    Basic sandbox

---
+++ README.md
@@ -0,0 +1,5 @@
+# tinker-sandbox
+
+Is the sandbox portion of Tinker. It takes a user's input through a `POST`
+request and mashes it all together into output.
+
---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var express = require('express'),
+	swig = require('swig'),
+	cons = require('consolidate'),
+	decode = require('base64').decode;
+
+var app = express()
+	.engine('html', cons.swig)
+	.set('view engine', 'html')
+	.set('views', __dirname + '/views')
+	.use(express.static(__dirname + '/public'))
+	.use(express.bodyParser());
+
+if (app.settings.env == 'development'){
+	swig.setDefaults({cache: false});
+}
+
+app.post('/', function(req, res){
+	var locals = {};
+	locals.tinker = {
+		code: {
+			html: decode(req.body.tinker.markup),
+			css: decode(req.body.tinker.style),
+			js: decode(req.body.tinker.behavior)
+		}
+	};
+	res.render('index', locals);
+});
+
+app.listen(4001, function(){
+	console.log('Listening on port 4001');
+});
+
